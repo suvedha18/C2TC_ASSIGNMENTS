3119c3d7bfcfedf11a98a26f9efa4ca13bfd764c
HP <HP@LAPTOP-463TH5EG>
2025-07-23 22:49:52 +0530

    this is my 2nd and day three assignment

---
+++ github.com/suvedha18/C2TC_ASSIGNMENTS/C2TC_ASSIGNMENTS/src/assignment2/StudentDemo.java
@@ -0,0 +1,15 @@
+package assignment2;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Student s1 = new Student("Suvedha", 20, "IT");
+
+	        // Calling method
+	        s1.displayDetails();
+
+	
+
+}
+}
