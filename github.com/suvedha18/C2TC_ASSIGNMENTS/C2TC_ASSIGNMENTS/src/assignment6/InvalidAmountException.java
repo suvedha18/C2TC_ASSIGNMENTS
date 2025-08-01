db514139dd3eeec2fd3c6aa685ddc913b1bce48f
HP <HP@LAPTOP-463TH5EG>
2025-08-01 11:17:56 +0530

    This is assignment6

---
+++ github.com/suvedha18/C2TC_ASSIGNMENTS/C2TC_ASSIGNMENTS/src/assignment6/InvalidAmountException.java
@@ -0,0 +1,7 @@
+package assignment6;
+
+public class InvalidAmountException extends Exception {
+    public InvalidAmountException(String message) {
+        super(message);
+    }
+}
--- github.com/suvedha18/C2TC_ASSIGNMENTS/C2TC_ASSIGNMENTS/src/daynine/StringSubClass.java
+++ github.com/suvedha18/C2TC_ASSIGNMENTS/C2TC_ASSIGNMENTS/src/daynine/StringSubClass.java
@@ -1,6 +1,7 @@
 package daynine;
 
-public class StringSubClass extends String {
+public class StringSubClass extends String
+{
 
 	public static void main(String[] args) {
 		
